397af40ba1f1d1621a4ce42b7bf06a57f26aca4a
Preet <preetaniltailor@gmail.com>
2017-01-26 23:50:32 +0300

    Preet Sample class pushed to github

---
+++ src/PreetSample.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Preet on 1/26/2017.
+ */
+public class PreetSample {
+    //We are going to push this class to github
+    //once you are done and saved your class just press control+K to conduct the push
+    public static void main(String[] args) {
+        System.out.println("Hello World!?");
+    }
+
+}
